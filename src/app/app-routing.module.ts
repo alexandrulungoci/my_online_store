655771059282a7d11f6b3c81807b3b60943e65e2
AlexLungoci <alexandru.lungoci@yahoo.com>
2020-10-01 23:30:29 +0300

    no message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OrderLineListComponent } from './order-line-list/order-line-list.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'product-list', component: ProductListComponent},
+  {path:'orderLine-list', component: OrderLineListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/order-line-list/order-line-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-line-list',
+  templateUrl: './order-line-list.component.html',
+  styleUrls: ['./order-line-list.component.css']
+})
+export class OrderLineListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
